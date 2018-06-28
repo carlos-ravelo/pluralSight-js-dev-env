7e9c0c35d9bd09df8d62b3314dfd18a09b0c6f2c
CRAVELO <cr.cravelo@gmail.com>
2018-06-28 08:48:47 -0400

    Initial checkin

---
+++ buildscripts/srcServer.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import { join } from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev'
+
+const port = 3000;
+const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}))
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, "../src/index.html"));
+}).listen(port, (err) => {
+    if (err) {
+        console.log(err)
+    }
+    else {
+        open('http://localhost:' + port)
+    }
+})
